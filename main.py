f1d6c67e443555090692a975f0fdf59f2f4ded4d
Arze Lu <arze2014@gmail.com>
2023-11-17 23:11:11 -0800

    edited main

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ import cc_serial as ccs
 from mpi4py import MPI
 
 def main():
-    #Get rank info
-    comm = MPI.COMM_WORLD
-    r = comm.Get_rank()
-    
     file_name = "facebook_combined.txt"
     
     #ccs_result = ccs.generate(file_name)
